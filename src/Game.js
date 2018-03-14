2313d8eef468725391e19303c584a3888f8fc59c
Ian Strouse <ian.strouse@gmail.com>
2018-03-13 19:20:41 -0600

    Add lodash

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 
 
 const Stars = (props) => {
+  const numberOfStars = 1 + Math.floor(Math.random()*9);
+  let starArr = [];
+
+  for (let i = 0; i < numberOfStars; i++) {
+    starArr.push(<i className="fa fa-star"></i>);
+  }
+
   return (
     <div>
-      <i className="fa fa-star"></i>
-      <i className="fa fa-star"></i>
-      <i className="fa fa-star"></i>
-      <i className="fa fa-star"></i>
+      {starArr}
     </div>
   )
 }
@@ -15,7 +20,7 @@ const Stars = (props) => {
 const Button = (props) => {
   return (
     <div>
-      ...
+      <button>=</button>
     </div>
   )
 }
@@ -28,14 +33,37 @@ const Answer = (props) => {
   )
 }
 
+const Numbers = (props) => {
+  const numberArray = _.range(1, 10);
+  console.log(numberArray);
+  return (
+    <div>
+      <div>
+        <span className="selected">1</span>
+        <span className="used">2</span>
+        <span>3</span>
+        <span>4</span>
+        <span>5</span>
+        <span>6</span>
+        <span>7</span>
+        <span>8</span>
+        <span>9</span>
+      </div>
+    </div>
+  )
+}
+
 class Game extends Component {
   render() {
     return (
-      <div>
-        <h3>Play 9 Game</h3>
-        <Stars />
-        <Button />
-        <Answer />
+      <div className="container">
+        <h1>Play 9 Game</h1>
+        <div className="row">
+          <Stars />
+          <Button />
+          <Answer />
+        </div>
+        <Numbers />
       </div>
     );
   }
