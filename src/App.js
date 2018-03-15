9e53b120b6d3e41e416665c5342e27a5f1c33213
Ian Strouse <ian.strouse@gmail.com>
2018-03-15 12:42:57 -0600

    Remove play9 from src

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import 'font-awesome/css/font-awesome.min.css';
 
-import Game from './Game';
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Game />
+        <p>Hello, world!</p>
       </div>
     );
   }
