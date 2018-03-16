35389c3431f2a69435a53c55e58a524ba01ba6d9
Jennifer Winer <jeniwiner@gmail.com>
2018-03-15 19:30:27 -0600

    button

---
+++ src/Button.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        <p>This is a button</p>
+      </div>
+    );
+  }
+}
+
+export default Button;
