79735ad385faa5a7ee4e493409cfcdb70d85e142
Joey R <joeyr84@gmail.com>
2018-03-15 20:07:46 -0600

    40 more minutes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'font-awesome/css/font-awesome.min.css';
+import Button from './Button.js';
 
-class App extends Component {
+export default class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {count: 0, index: 0, sequence: [0,1,3], playersTurn: false}
+
+  }
+  
+  youLose(){
+
+  }
+
+  nextSeq() {
+    //increment count and seq and lightup
+  }
+  onButtonClick(btn){
+    if (btn == this.state.sequence[this.state.index]){
+      let index = this.state.index + 1
+      this.setState({index: index})
+      if(index == this.state.sequence.length) {
+        nextSeq()
+      }
+    } 
+    else{
+      youLose()
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <p>Hello, world!</p>
+        <Button onClick={()=>this.onButtonCLick(0)} />
+        <Button onClick={()=>this.onButtonCLick(1)} />
+        <Button onClick={()=>this.onButtonCLick(2)} />
+        <Button onClick={()=>this.onButtonCLick(3)} />
+        <Start />
+        <Counter />
       </div>
     );
   }
 }
-
-export default App;
+check index vs sequence, light up
+need an onclick tht passes down to button, checks sequence, start that init sequence
